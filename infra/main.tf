a8239b6389cfa51b1737e532e8b0cdff3f5e5b44
ManvithaBhat <106172510+ManvithaBhat@users.noreply.github.com>
2025-09-24 16:14:43 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -10,13 +10,14 @@ resource "google_container_node_pool" "primary_nodes" {
   name     = "primary-node-pool"
   cluster  = google_container_cluster.primary.name
   location = var.zone
+  node_count = 2
 
   node_config {
     machine_type = "e2-medium"
+    disk_size_gb = 30
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
   }
 
-  initial_node_count = 2
 }
